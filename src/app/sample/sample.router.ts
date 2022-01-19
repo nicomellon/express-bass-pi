b570bce13d677a7d190509059ce3787a371b0e18
nicomellon <nmellon94@gmail.com>
2022-01-19 21:09:15 +0100

    initial setup

--- src/app/sample/sample.router.ts
+++ src/app/sample/sample.router.ts
@@ -1,31 +1,29 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 // Export module for registering router in express app
 export const router: Router = Router();
 
 // Define your routes here
-router.get("/", (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.status(200).send({
-    message: "GET request from sample router"
+    message: 'GET request from sample router',
   });
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req: Request, res: Response) => {
   res.status(200).send({
-    message: "POST request from sample router"
+    message: 'POST request from sample router',
   });
 });
 
-router.put("/", (req, res) => {
+router.put('/', (req: Request, res: Response) => {
   res.status(200).send({
-    message: "PUT request from sample router"
+    message: 'PUT request from sample router',
   });
 });
 
-router.delete("/", (req, res) => {
+router.delete('/', (req: Request, res: Response) => {
   res.status(200).send({
-    message: "DELETE request from sample router"
+    message: 'DELETE request from sample router',
   });
 });
-
-
