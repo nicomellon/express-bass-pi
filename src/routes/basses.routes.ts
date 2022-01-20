c131821069cdc0739c756982661ecdeb3a668ae6
nicomellon <nmellon94@gmail.comm>
2022-01-20 11:58:32 +0100

    add postBasses

--- src/routes/basses.routes.ts
+++ src/routes/basses.routes.ts
@@ -3,11 +3,28 @@ import { db } from '../db';
 
 export const bassesRouter: Router = express.Router();
 
-bassesRouter.route('/').get((req: express.Request, res: express.Response) => {
-  db.query('SELECT * FROM basses', (error, results, fields) => {
-    if (error) {
-      console.error(error);
-    }
+const getAllBasses = (req: express.Request, res: express.Response) => {
+  const query: string = 'SELECT * FROM basses';
+  db.query(query, (error, results, fields) => {
+    if (error) console.error(error);
     res.json(results);
   });
-});
+};
+
+const postBasses = (req: express.Request, res: express.Response) => {
+  const { manufacturerId, name, launchYear, image } = req.body;
+
+  const query: string =
+    'INSERT INTO basses (manufacturer_id, name, launch_year, image) VALUES (?, ?, ?, ?);';
+  const inserts = [manufacturerId, name, launchYear, image];
+
+  db.query(query, inserts, (error, results, fields) => {
+    if (error) console.error(error);
+    res.json(results);
+  });
+};
+
+bassesRouter
+  .route('/') // -> /api/basses
+  .get(getAllBasses)
+  .post(postBasses);
