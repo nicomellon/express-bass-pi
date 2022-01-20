588d015ff8362676bf40f3c2f3549c54a05d6bc5
nicomellon <nmellon94@gmail.comm>
2022-01-20 12:47:23 +0100

    adds postManufacturer

--- src/routes/manufacturers.routes.ts
+++ src/routes/manufacturers.routes.ts
@@ -5,14 +5,6 @@ export const manufacturersRouter: Router = express.Router();
 
 /* handler functions */
 
-const getAllManufacturers = (req: express.Request, res: express.Response) => {
-  const query: string = 'SELECT * FROM manufacturers';
-  db.query(query, (error, results, fields) => {
-    if (error) console.error(error);
-    res.status(200).json(results);
-  });
-};
-
 const getManufacturerByID = (req: express.Request, res: express.Response) => {
   const manufacturerID = req.params.id;
   const query: string = `SELECT * FROM manufacturers WHERE id = ${manufacturerID}`;
@@ -23,6 +15,26 @@ const getManufacturerByID = (req: express.Request, res: express.Response) => {
   });
 };
 
+const getAllManufacturers = (req: express.Request, res: express.Response) => {
+  const query: string = 'SELECT * FROM manufacturers';
+  db.query(query, (error, results, fields) => {
+    if (error) console.error(error);
+    res.status(200).json(results);
+  });
+};
+
+const postManufacturers = (req: express.Request, res: express.Response) => {
+  const { name, foundedYear, nationality, logo } = req.body;
+
+  const query: string = `INSERT INTO manufacturers (name, founded_year, nationality, logo) 
+    VALUES ('${name}', ${foundedYear}, '${nationality}', ${logo});`;
+
+  db.query(query, (error, results, fields) => {
+    if (error) console.error(error);
+    res.status(201).json(results);
+  });
+};
+
 /* router */
 manufacturersRouter
   .route('/:id') // -> /api/manufacturers/:id
@@ -30,4 +42,5 @@ manufacturersRouter
 
 manufacturersRouter
   .route('/') // -> /api/manufacturers
-  .get(getAllManufacturers);
+  .get(getAllManufacturers)
+  .post(postManufacturers);
